78d085635518409b3cf50325f4d23e1b4fd65957
francescafr <hello@francesca.fyi>
2023-02-08 16:05:54 -0800

    created base models and textures, added to world scene

--- terrarium/src/components/threeJS/Scene/Lights/index.js
+++ terrarium/src/components/threeJS/Scene/Lights/index.js
@@ -10,7 +10,7 @@ const Lights = () => {
 
   return (
     <group>
-      <FakeSphere />
+      {/* <FakeSphere /> */}
       <ambientLight position={[0, 4, 0]} intensity={0.3} />
       <directionalLight intensity={0.5} position={[0, 0, 0]} color={0xffffff} />
       <pointLight
